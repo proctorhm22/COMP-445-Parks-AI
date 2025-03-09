336cd7af51d0992f38a04fa7fde7b58e82faa877
proctorhm22 <proctorhm22@gcc.edu>
2025-03-08 21:08:48 -0500

    Added Park class / changed main a lot.

--- foward_checking.py
+++ foward_checking.py
@@ -17,6 +17,7 @@ Output: solution = {{()}, ..., {()}}
 
 """
 
+import copy
 
 def start_search(n, num_trees, variables, domains):
     done = set()
@@ -25,26 +26,27 @@ def start_search(n, num_trees, variables, domains):
 
 """
 0 <= next_var <= 3n-1
-choose next_var based on shortest domain...
+choose next_var based on shortest domain
 """
 def foward_checking(n, num_trees, variables, domains, done, next_var):
     # if each domain in domains has size 1 (num_trees): return domains
-    # allAssigned = True
-    # for domain in domains:
-    #     if len(domain) > 1:
-    #         allAssigned = False
-    #         break
-    # if allAssigned:
+    allAssigned = True
+    for domain in domains:
+        if len(domain) > num_trees:
+            allAssigned = False
+            break
+    if allAssigned:
+        return {set.pop() for set in domains[0:n]}
+    # if len(done) == 3*n:
     #     return [set.pop() for set in domains[0:n]]
-    if len(done) == 3*n:
-        return [set.pop() for set in domains[0:n]]
     # print(f"next_var = {next_var}")
     # else run through values in domains[next_var]
     values_to_try = domains[next_var]
     # print(f"values_to_try = {values_to_try}")
     for value in values_to_try:
         # print(f"value = {value}")
-        new_domains = domains[:]
+        # new_domains = domains[:]
+        new_domains = copy.copy(domains)
         # new_domains[next_var] = {value}
         # print(f"domains = {domains}")
         new_domains = constraint_propogation(n, num_trees, variables, new_domains, value)
@@ -55,13 +57,11 @@ def foward_checking(n, num_trees, variables, domains, done, next_var):
             if len(domain) == 0:
                 hasEmptyDomain = True
                 break
-                # return None
-        if hasEmptyDomain:
-            pass
         if not hasEmptyDomain:
             # print("NO EMPTY DOMAINS")
             # print(f"done = {done}")
-            new_done = done | set()
+            # new_done = done | set()
+            new_done = copy.copy(done)
             new_done.add(next_var)
             # print(f"new_done = {done}")
             result = foward_checking(n, num_trees, variables, new_domains, new_done, pick_next_var(n, variables, new_domains, new_done)) 	
@@ -93,17 +93,13 @@ value = (i, j)
 0 <= next_var <= 3n-1
 """
 def constraint_propogation(n, num_trees, variables, domains, value):
-    # domains = new_domains
     i = value[0]
     j = value[1]
-    if num_trees == 1:
-        for k in range(0, 3*n):
-            if (i, j) in domains[k]:
-                domains[k] = {(i, j)}
-            else:
-                domains[k] = {coord for coord in domains[k] if coord[0] != i and coord[1] != j}
-                domains[k] = domains[k] - {(i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)}
-    else:
-        pass
+    for k in range(0, 3*n):
+        if (i, j) in domains[k]:
+            domains[k] = {(i, j)}
+        else:
+            domains[k] = {coord for coord in domains[k] if coord[0] != i and coord[1] != j}
+            domains[k] = domains[k] - {(i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)}
     return domains
     
--- main.py
+++ main.py
@@ -1,162 +1,128 @@
+"""
+Authors:    Jacob Huber / Hannah Proctor
+Assignment: COMP 445 Research Project
+Date:       March 10, 2025
+
+Generates / solves Parks problem instances from Logic Games app.
+"""
+
 import foward_checking as fc
 import time
 
-"""
-variables = [row1, ..., rown, col1, ..., coln, color1, ..., colorn] 
-          = [0, ..., n-1, n, ..., 2n-1, 2n, ..., 3n-1]
-
-domains = [{(0, 0), ..., (0, n-1)}, ..., {(n-1, 0), ..., (n-1, n-1)}, # rows
-                {(0, 0), ..., (n-1, 0)}, ..., {(0, n-1), ..., (n-1, n-1)} # cols
-                {...}, ..., {...}] # colors
-"""
-def main():
-    problem_instances = get_problem_instances()
-    print()
-    for instance in problem_instances:
-        n = instance[0]
-        num_trees = instance[1]
-        variables = instance[2]
-        domains = instance[3]
-        colors = instance[4]
-        start = time.perf_counter()
-        solution = fc.start_search(n, num_trees, variables, domains)
-        end = time.perf_counter()
-        print(f"solution = {solution}")
-        print(f"{end-start:0.6f}s")
-        print()
-        print_board(solution, colors)
-        print()
-
-
-"""
-used in get_problem_instances()... makes it easier to input game board data
-"""
-def read_colors(colors: str, n: int) -> dict:
-    colors_dict = dict()
-    colors = colors.split()
-    # print(colors)
-    for k in range(0, len(colors)):
-        temp = colors_dict.get(colors[k], set())
-        i = int(k / n)
-        j = int(k % n)
-        # print(f"colors[{k}]: ({i}, {j})")
-        temp.add((i, j))
-        colors_dict[colors[k]] = temp
-    # print(colors_dict)
-    # return list(colors_dict.values())
-    return colors_dict
-
+class Park():
+    def __init__(self, level, n, num_trees, color_str):
+        self.level = level
+        self.n = n
+        self.num_trees = num_trees
+        self.variables = [i for i in range(0, 3*n)]
+        # self.variables = [(i, j) for i in range(0, n) for j in range (0, n)]
+        # print(self.variables)
+        self.colors = read_colors(color_str, n) 
+        # print(self.colors)
+        self.domains = [{(i, j) for j in range(0, n)} for i in range(0, n)] # rows
+        self.domains.extend([{(j, i) for j in range(0, n)} for i in range(0, n)]) # columns
+        self.domains.extend(list(self.colors.values())) # colors
+        # self.domains = {(i, j): {True, False} for (i, j) in self.variables}
+        # print(self.domains)
+        self.solution = None
+    def __str__(self):
+        s = ""
+        for i in range(0, self.n):
+            for j in range(0, self.n):
+                k = "?"
+                for color in self.colors.keys():
+                    if (i, j) in self.colors[color]:
+                        k = color
+                if self.solution != None and (i, j) in self.solution:
+                # if self.domains.get((i, j)) == {True}:
+                    s = s + f"{k} TREE\t"
+                else:
+                    s = s + f"{k} ....\t"
+            s = s + "\n\n"
+        s = s.strip()
+        return s
+    
 
 """
-generates instances for level 1 (n=5), level 10 (n=6), and level 14 (n=7)
-(n, num_trees, variables, domains, colors)
-"""
-def get_problem_instances() -> list:
-    probs = []
-    # LEVEL 1
-    n = 5
-    num_trees = 1
-    variables = [i for i in range(0, 3*n)]
-    # print(variables)
-    domains = [{(i, j) for j in range(0, n)} for i in range(0, n)] # rows
-    domains.extend([{(j, i) for j in range(0, n)} for i in range(0, n)]) # columns
-    s = """
+Given color_str of the form:
     B B V G M
     B B V V M
     Y B V M M
     Y Y Y M M
     Y Y Y Y Y
-    """
-    colors = read_colors(s, n)
-    domains.extend(list(colors.values()))
-    # print(domains)
-    probs.append((n, num_trees, variables, domains, colors))
-    # LEVEL 10
-    n = 6
-    num_trees = 1
-    variables = [i for i in range(0, 3*n)]
-    # print(variables)
-    domains = [{(i, j) for j in range(0, n)} for i in range(0, n)] # rows
-    domains.extend([{(j, i) for j in range(0, n)} for i in range(0, n)]) # columns
-    # domains = [{(i, j, False) for j in range(0, n)} for i in range(0, n)] # rows
-    # domains.extend([{(j, i, False) for j in range(0, n)} for i in range(0, n)]) # columns
-    s = """
+Return color_dict of the form:
+    {"B":{(i, j) | (i, j) is blue}, "V":{(i, j) | (i, j) is green}, ... "G":{(i, j) | (i, j) is gray}}
+"""
+def read_colors(colors_str: str, n: int) -> dict[str, set]:
+    colors_dict = dict()
+    # split color_str into color_list: ["B", "B", "V", "G", "M", "B", "B", "V", "V", "M", ..., "Y"]
+    colors_list = colors_str.split()
+    # iterate over color_list to build dictionary
+    for k in range(0, len(colors_list)):
+        temp = colors_dict.get(colors_list[k], set())
+        i = int(k / n)
+        j = int(k % n)
+        temp.add((i, j))
+        colors_dict[colors_list[k]] = temp
+    return colors_dict
+
+
+def main():
+    parks = []
+    parks.append(Park(level="LEVEL 1", n=5, num_trees=1, color_str="""
+    B B V G M
+    B B V V M
+    Y B V M M
+    Y Y Y M M
+    Y Y Y Y Y
+    """))
+    parks.append(Park(level="LEVEL 10", n=6, num_trees=1, color_str="""
     B B V V V V
     B V V G M V
     B V V G M M
     Y Y G G P M
-    Y V G P P M
+    Y G G P P M
     Y Y Y P P P
-    """
-    colors = read_colors(s, n)
-    domains.extend(list(colors.values()))
-    # print(domains)
-    probs.append((n, num_trees, variables, domains, colors))
-    # LEVEL 14
-    n = 7
-    num_trees = 1
-    variables = [i for i in range(0, 3*n)]
-    # print(variables)
-    domains = [{(i, j) for j in range(0, n)} for i in range(0, n)] # rows
-    domains.extend([{(j, i) for j in range(0, n)} for i in range(0, n)]) # columns
-    # domains = [{(i, j, False) for j in range(0, n)} for i in range(0, n)] # rows
-    # domains.extend([{(j, i, False) for j in range(0, n)} for i in range(0, n)]) # columns
-    s = """
+    """))
+    parks.append(Park(level="LEVEL 14", n=7, num_trees=1, color_str="""
     B B B B V V V
     B B G B M M V
-    B Y G P P M G
+    B Y G P P M V
     B Y G G P M M
     B Y Y G P T T
     B B T T T T T
     T T T T T T T
-    """
-    colors = read_colors(s, n)
-    domains.extend(list(colors.values()))
-    # print(domains)
-    probs.append((n, num_trees, variables, domains, colors))
-    # LEVEL 15
-    n = 9
-    num_trees = 1
-    variables = [i for i in range(0, 3*n)]
-    # print(variables)
-    domains = [{(i, j) for j in range(0, n)} for i in range(0, n)] # rows
-    domains.extend([{(j, i) for j in range(0, n)} for i in range(0, n)]) # columns
-    s = """
-    B B B B B B B B B
-    V V B B G B G G M
-    V V V G G G G M M
-    V V V Y Y P M M M
-    V V V V Y P P P M
-    V D Y Y Y L P P M
-    D D D L L L P P M
-    D D F F F L L P P
-    D D F F F F L P P
-    """
-    colors = read_colors(s, n)
-    domains.extend(list(colors.values()))
-    # print(domains)
-    probs.append((n, num_trees, variables, domains, colors))
-    return probs
-
-
-"""
-prints a board-looking solution, instead of just coordinates
-"""
-def print_board(solution: set, colors: dict) -> None:
-    s = ""
-    for i in range(0, len(colors)):
-        for j in range(0, len(colors)):
-            k = "?"
-            for color in colors.keys():
-                if (i, j) in colors[color]:
-                    k = color
-            if (i, j) in solution:
-                s = s + f"{k} TREE\t"
-            else:
-                s = s + f"{k} ....\t"
-        s = s + "\n\n"
-    s = s.strip()
-    print(s)
+    """))
+    parks.append(Park(level="LEVEL 16", n=8, num_trees=2, color_str="""
+    B V V V V L L L
+    B V V L L L L L
+    B V V L M L L L
+    B B M M M Y Y Y
+    B M M M P Y D Y
+    B T M P P Y D Y
+    T T T P P P D D
+    T T D D D D D D
+    """))
+    parks.append(Park(level="LEVEL 17", n=9, num_trees=2, color_str="""
+    B B V V V V V L L
+    B M M M V V V L L
+    B M Y Y Y L L L L
+    B M M M Y P D L L
+    T T T M G P D D D
+    T M M M G P P P D
+    T T T G G P P P D
+    T G T G G P P P D
+    G G G G G G D D D
+    """))
+    for park in parks:
+        print(f"\n{park.level}\n")
+        start = time.perf_counter()
+        park.solution = fc.start_search(park.n, park.num_trees, park.variables, park.domains)
+        end = time.perf_counter()
+        print(park.solution)
+        print(f"{end-start:0.6f}s")
+        print(f"\n{park}\n")
+    print()
 
 
 if __name__ == "__main__":
