8a2a215d22140c697f87211d1236f1e4f3ca7ba0
HUBERJW22 <huberjw22@gcc.edu>
2025-03-10 01:41:20 -0400

    Constraint Learning with up to date forward checking

--- constraint_learning.py
+++ constraint_learning.py
@@ -6,7 +6,7 @@ Date:           March 10, 2025
 Implementation of foward-checking algorithm to solve Parks problem instances with 1-2 trees**. Code is based on
 the forward checking algorithm and uses a modified implementation of constraint learning.
 
-Additional constraints are "learned" by relating variables in a subset.
+Additional constraints are "learned" by relating variables in a subtree.
 The base is determined o consistent children and a "base" o
 
 **Currently, all 2-tree problems are solved as if they are 1-tree problems. 
@@ -29,13 +29,23 @@ def start_learning_search(park: Park, verbosity=0):
 
 def learning_forward_checking(n: int, num_trees: int, variables: set, domains: list, done: set, next_var: int, park: Park, 
                         verbosity: int, learning: set, base_var: int, prev_var: int):
-    # if len(done) == len(variables): return domains
-    #print("learned set: " + str(learning))
-    if len(done) == 3*n:
+    # if each domain in domains has size 1 (num_trees): return solution
+    all_assigned = True
+    for domain in domains:
+        # if len(domain) != num_trees:
+        if len(domain) != 1:
+            all_assigned = False
+    if all_assigned:
+        # increment num_nodes_explored (algorithm still "explores" the current node)
+        park.num_nodes_explored = park.num_nodes_explored + 1
+        # # (formatting main output)
         if verbosity > 0:
-            print()
-        return [domain.pop() for domain in domains[0:n]], park.num_nodes_explored
-    # check if current variables violate a learned constraint
+            print(f"next_var = {next_var}")
+            print(f"\ttry {copy.copy(domains[next_var]).pop()}; {park.num_nodes_explored} nodes explored\n")
+            # print()
+        # return solution
+        return [domain.pop() for domain in domains[0:n]]
+    # else: assign value to next_var# check if current variables violate a learned constraint
     if len(learning) != 0 and prev_var != None:
         for x in range(len(domains)):
             if(len(domains[x]) == 1):
@@ -45,34 +55,42 @@ def learning_forward_checking(n: int, num_trees: int, variables: set, domains: l
                     if verbosity > 0:
                         print("I LEARNED SOMETHING")
                     return None
-        #print("done checking for now")
-    # else: iterate through values in domains[next_var]
     values_to_try = domains[next_var]
     for value in values_to_try:
-        # try it
-        # new_domains[next_var] = {value}
+        # try it: new_domains[next_var] = {value}
+        # increment number of nodes explored 
         park.num_nodes_explored = park.num_nodes_explored + 1
         if verbosity > 0:
             print(f"next_var = {next_var}")
             print(f"\ttry {value}; {park.num_nodes_explored} nodes explored")
+        # new_domains = copy of domains
         new_domains = copy.copy(domains)
+        # run constraint_propogation() on new_domains to detect inconsistencies before recursion
         new_domains = learning_constraint_propagation(n, num_trees, variables, new_domains, value)
         if verbosity == 2:
             print(f"domains = {domains}")
             print(f"new_domains = {new_domains}")
-        # if new_domains has no empty domains: ...
+        # detect inconsistencies...
         noEmptyDomains = True
         for domain in new_domains:
             if len(domain) == 0:
             # if len(domain) < num_trees:
                 noEmptyDomains = False
                 break
+        # if new_domains has no empty domains...
         if noEmptyDomains:
+            # new_done = copy of done set
             new_done = copy.copy(done)
             new_done.add(next_var)
+            # add variables with size 1 (num_tree) domains to new_done
+            for k in variables:
+                if len(new_domains[k]) == 1 and new_domains[k] == {value}:
+                    new_done.add(k)
+            # make recursive call
             result = learning_forward_checking(n, num_trees, variables, new_domains, new_done, 
                                      learning_pick_next_var(n, variables, new_domains, new_done), park,
                                        verbosity, learning, base_var, next_var) 	
+            # define start of subtree
             base_var = next_var
             if result is not None:
                 return result
@@ -93,17 +111,22 @@ def learning_forward_checking(n: int, num_trees: int, variables: set, domains: l
 Strategically picks next variable to try (minimum remaining value).
 """
 def learning_pick_next_var(n, variables, domains, done):
+    # find first variable that hasn't been assigned
     i = 0
     while variables[i] in done and i < 3*n - 1:
         i = i + 1
+    # initialize next_var to this variable / get baseline MRV (min_len)
     next_var = i
     min_len = len(domains[i])
+    # loop through remaining variables
     for j in range(i, 3*n):
+        # if one of them is also unassigned / has a "better" MRV
         if variables[j] not in done and len(domains[j]) < min_len:
+            # update next_var 
             next_var = j
             min_len = len(domains[j])
+    # returns variable with "lowest" MRV
     return next_var
-
 
 """
 Runs constraint propogation on the given set of domains. 
@@ -114,11 +137,14 @@ def learning_constraint_propagation(n, num_trees, variables, domains, value):
     # value = (i, j)
     i = value[0]
     j = value[1]
-    for k in range(0, 3*n):
-        if value in domains[k]:
-            domains[k] = {value}
+    for k in variables:
+        if (i, j) in domains[k]:
+            # place tree
+            domains[k] = {(i, j)}
         else:
+            # clear rest of row / column
             domains[k] = {coord for coord in domains[k] if coord[0] != i and coord[1] != j}
+            # clear surrounding cells
             domains[k] = domains[k] - {(i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)}
     return domains
     
